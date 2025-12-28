328fa1ec0986ba0703cd81ff0ffc9bacb38889aa
Aakash Acharya <mail.aakash108@gmail.com>
2025-12-28 16:10:11 +1100

    useeffect in use

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
-import { data } from "../data"
 
 const Main = () => {
-    const [value, setValue] = React.useState(data)
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [value, setValue] = React.useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    function handleChange(event) {
+        console.log(event.currentTarget)
+        const { value, name } = event.currentTarget
+        setValue(prevData => ({ ...prevData, [name]: value }))
+    }
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setStarWarsData(data.data.memes))
+
+    }, [])
+    function handleButtonClick() {
+        const randomItem = starWarsData[Math.floor(Math.random() * starWarsData.length)];
+        setValue(prevData=> ({...prevData, imageUrl: randomItem.url}))
+
+    }
+
+
+
+
     return (
         <main>
+
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        placeholder="One does not si mply"
                         name="topText"
+                        onChange={handleChange}
+                        value={value.topText}
                     />
                 </label>
 
@@ -19,23 +47,24 @@ const Main = () => {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={value.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={handleButtonClick}>Get a new meme image 🖼</button>
             </div>
 
             {
-                value.map((item) => {
-                    return (
-                        <div className="meme" key={item.id}>
-                            <img src={item.imageUrl} />
-                            <span className="top">{item.topText}</span>
-                            <span className="bottom">{item.bottomText}</span>
-                        </div>
 
-                    )
+                <div className="meme" key={value.id}>
+                    <img src={value.imageUrl} />
+                    <span className="top">{value.topText}</span>
+                    <span className="bottom">{value.bottomText}</span>
+                </div>
 
-                })
+
+
+
             }
 
 
--- src/data.jsx
+++
@@ -1,8 +0,0 @@
-export  const data = [
-    {
-        id: 1,
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
-    }
-]
